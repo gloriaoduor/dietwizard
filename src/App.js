f944e1ccd5cfc72dd36b47445d1122e6cf82a80b
Developer Rodger <rodgermethuselah@cim-pool.ch>
2024-04-19 16:00:46 +0300

    editted the layout

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./App.css";
+import BioDataForm from "./components/BioDataForm";
+import dietImg from "./assets/diet3.svg";
+
+function App() {
+  const [showForm, setShowForm] = useState(false);
+  const handleGetStarted = () => {
+    setShowForm(true);
+  };
+  return (
+    <div className="min-vh-100 ">
+      <div className="landing-page">
+        {/* <div className="background-image"></div> */}
+        <div className="container content">
+          {!showForm ? (
+            <div>
+              <div className="text-section">
+                <h1 className="font mb-5">DIET WIZARD</h1>
+                <img
+                  src={dietImg}
+                  alt="diet"
+                  className="w-50 mx-auto d-block my-4"
+                />
+                <p className="body-font">
+                  <h5
+                    style={{ textAlign: "center", textJustify: "inter-word" }}
+                  >
+                    {/* Simplifies healthy eating by providing personalized guidance
+                    based
+                    <br />
+                    on your unique needs, ensuring a balanced diet tailored to
+                    your goals.
+                    <br /> */}
+                    Whether you want to maintain, gain, or lose weight, this app
+                    helps you <br />
+                    make informed choices for a healthier lifestyle.
+                  </h5>
+                </p>
+                <button
+                  className="btn btn-lg text-white"
+                  style={{ backgroundColor: "#121f57" }}
+                  onClick={handleGetStarted}
+                >
+                  Get Started
+                </button>
+              </div>
+            </div>
+          ) : (
+            <BioDataForm /> // Your analysis form component
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BioDataForm.js
@@ -0,0 +1,242 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { calculateBmr } from "./AnalysisFxn";
+import "../App.css";
+
+const BioDataForm = () => {
+  const [formData, setFormData] = useState({
+    name: "Glo",
+    age: "25",
+    gender: "female",
+    height: "1.69",
+    weight: "51",
+    activityLevel: "Sedentary",
+  });
+  const [response, setResponse] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const result = calculateBmr(
+      formData.gender,
+      formData.weight,
+      formData.height,
+      formData.age,
+      formData.activityLevel
+    );
+    setResponse(result);
+    console.log(formData);
+  };
+
+  const clearForm = (e) => {
+    setFormData({
+      name: "",
+      age: "",
+      gender: "",
+      height: "",
+      weight: "",
+      activityLevel: "",
+    });
+    setResponse(null);
+  };
+
+  return (
+    <div className="container padded font">
+      <div className="card border rounded shadow">
+        <div className="card-body">
+          {response ? (
+            // Display the response if available
+            <div className="row" role="alert">
+              <h4 className="font">DIET WIZARD</h4>
+              <h5>
+                <strong>BIODATA:</strong>
+              </h5>
+              <div className="row">
+                <div className="col">
+                  <p>
+                    <strong>NAME: </strong> {formData.name}
+                  </p>
+                  <p>
+                    <strong>AGE: </strong> {formData.age}
+                  </p>
+                  <p>
+                    <strong>GENDER: </strong>{" "}
+                    {formData.gender.toLocaleUpperCase().charAt(0)}
+                  </p>
+                  <p>
+                    <strong>BMI: </strong> {response.bmi}
+                  </p>
+                  <p>
+                    <strong>CATEGORY: </strong> {response.category}
+                  </p>
+                  <p>
+                    <strong>BMR: </strong> {response.bmr}
+                  </p>
+                </div>
+              </div>
+
+              <h4>
+                <strong>RESULTS: </strong>
+              </h4>
+              <table className="table-responsive table-bordered">
+                <thead>
+                  <tr>
+                    <th>Food Group</th>
+                    <th>Portion</th>
+                    <th>Total Calories</th>
+                    <th>Food Recommendations</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(response.foodPortions).map(
+                    (groupName, index) => (
+                      <tr key={index}>
+                        <td>{groupName}</td>
+                        <td>{response.foodPortions[groupName].portion}</td>
+                        <td>{response.foodPortions[groupName].calories}</td>
+                        <td>{response.selectedRecommendations[groupName]}</td>
+                      </tr>
+                    )
+                  )}
+                  <tr>
+                    <td colSpan="2">
+                      <strong>Total Calories</strong>
+                    </td>
+                    <td>
+                      <strong>{response.caloriesQty}</strong>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              {/* Display other properties as needed */}
+              <button
+                className="btn d-block text-white mt-2"
+                style={{ backgroundColor: "#880808" }}
+                onClick={clearForm}
+              >
+                Clear
+              </button>
+            </div>
+          ) : (
+            <form onSubmit={handleSubmit} className="row p-4 rounded">
+              <h5 className="font">DIET WIZARD</h5>
+              <h4 className=" mb-4 font">Analysis Form</h4>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label"></label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="age"
+                  name="age"
+                  placeholder="Age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <select
+                  className="form-select"
+                  id="gender"
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="height"
+                  name="height"
+                  placeholder="Height(m)"
+                  value={formData.height}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="weight"
+                  name="weight"
+                  placeholder="Weight (kg)"
+                  value={formData.weight}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="mb-3">
+                <select
+                  className="form-select"
+                  id="activityLevel"
+                  name="activityLevel"
+                  value={formData.activityLevel}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Activity Level</option>
+                  <option value="Sedentary">
+                    Sedentary (little or no exercise)
+                  </option>
+                  <option value="Light">
+                    Lightly active (light exercise or sports 1-3 days a week)
+                  </option>
+                  <option value="Moderate">
+                    Moderately active (moderate exercise or sports 3-5 days a
+                    week)
+                  </option>
+                  <option value="Very">
+                    Very active (hard exercise or sports 6-7 days a week)
+                  </option>
+                  <option value="Super">
+                    Super active (very hard exercise and physical job or 2x
+                    training)
+                  </option>
+                </select>
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-block text-white "
+                style={{ backgroundColor: "#121f57" }}
+              >
+                Submit
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BioDataForm;
